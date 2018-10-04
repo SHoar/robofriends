deb62b0559f69e84a91b33645f6bc1bfd588590a
Sean <sean.hoar@gmail.com>
2018-10-04 08:02:27 -0400

    add jest & enzyme test coverage

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -21,8 +21,13 @@ export default class CounterButton extends Component {
         })
     }
     render(){
-        console.log('CounterButton')
-        return <button color={this.props.color} onClick={this.updateCount} >Count: {this.state.count}</button>
+        // console.log('CounterButton')
+        return <button 
+                    id='counter'
+                    color={this.props.color} 
+                    onClick={this.updateCount} >
+                        Count: {this.state.count}
+               </button>
     }
 }
 
